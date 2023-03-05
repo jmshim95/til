beb5abeb18b4bd43a03afd6c67b3f87ae528b728
jmshim95 <jmshim95@gmail.com>
2023-03-05 11:51:40 +0900

    boj_10171

---
+++ level_01/Main_10171.java
@@ -0,0 +1,18 @@
+public class Main_10171 {
+
+    public static void main(String[] args) {
+        /*
+            \    /\
+             )  ( ')
+            (  /  )
+             \(__)|
+         */
+        StringBuilder sb = new StringBuilder();
+        sb.append("\\    /\\\n");
+        sb.append(" )  ( ')\n");
+        sb.append("(  /  )\n");
+        sb.append(" \\(__)|\n");
+
+        System.out.println(sb);
+    }
+}
