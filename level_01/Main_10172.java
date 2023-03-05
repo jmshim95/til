2ed6a5d7cf39c6c0fc4a3202330b6f8505bde78a
jmshim95 <jmshim95@gmail.com>
2023-03-05 11:54:52 +0900

    boj_10172

---
+++ level_01/Main_10172.java
@@ -0,0 +1,20 @@
+public class Main_10172 {
+
+    public static void main(String[] args) {
+        /*
+|\_/|
+|q p|   /}
+( 0 )"""\
+|"^"`    |
+||_/=\\__|
+         */
+        StringBuilder sb = new StringBuilder();
+        sb.append("|\\_/|\n");
+        sb.append("|q p|   /}\n");
+        sb.append("( 0 )\"\"\"\\\n");
+        sb.append("|\"^\"`    |\n");
+        sb.append("||_/=\\\\__|\n");
+
+        System.out.println(sb);
+    }
+}
