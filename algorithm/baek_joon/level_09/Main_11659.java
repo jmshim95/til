76476154a9db835259f433f3e4d1f773a1d5e7fe
jmshim95 <jmshim95@gmail.com>
2023-03-13 20:53:49 +0900

    boj_11659 시간초과

---
+++ algorithm/baek_joon/level_09/Main_11659.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main_11659 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int[] numbers = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int temp = 0;
+            for (int j = a-1; j < b; j++) {
+                temp += numbers[j];
+            }
+            sb.append(temp).append("\n");
+        }
+
+        bw.write(sb.toString());
+        br.close();
+        bw.close();
+    }
+}
