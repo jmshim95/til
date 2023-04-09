37e480bcf5ab866ed391327b42f2ffcbaf316a72
jmshim95 <jmshim95@gmail.com>
2023-04-09 15:20:34 +0900

    boj_2903

--- algorithm/baek_joon/level_08/Main_2903.java
+++ algorithm/baek_joon/level_08/Main_2903.java
@@ -6,18 +6,14 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
-        int a = 1;
-        int b = 2;
-        int c = 3;
-        int result = 4;
+        int a = 2;
+        int b = 1;
         for (int i = 0; i < n; i++) {
-            result += (a * b) + (c * a);
-            c += a * 2;
-            b += a;
-            a *= 2;
+            a += b;
+            b *= 2;
         }
 
-        bw.write(String.valueOf(result));
+        bw.write(String.valueOf((int) Math.pow(a, 2)));
         br.close();
         bw.close();
     }
