92d003193f11572f95936f8ee5f25c1f0e16fe1e
jmshim95 <jmshim95@gmail.com>
2023-04-23 22:41:27 +0900

    boj_9063

---
+++ algorithm/baek_joon/level_10/Main_9063.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class Main_9063 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int max_x = Integer.MIN_VALUE;
+        int max_y = Integer.MIN_VALUE;
+        int min_x = Integer.MAX_VALUE;
+        int min_y = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            String[] s = br.readLine().split(" ");
+            int x = Integer.parseInt(s[0]);
+            int y = Integer.parseInt(s[1]);
+
+            max_x = Math.max(max_x, x);
+            max_y = Math.max(max_y, y);
+            min_x = Math.min(min_x, x);
+            min_y = Math.min(min_y, y);
+        }
+
+        bw.write(String.valueOf(Math.abs(max_x - min_x) * Math.abs(max_y - min_y)));
+        br.close();
+        bw.close();
+    }
+}
