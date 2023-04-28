2fed36c8c0b152ab165cb08b44754459af13c739
jmshim95 <jmshim95@gmail.com>
2023-04-28 22:06:04 +0900

    boj_24263

---
+++ algorithm/baek_joon/level_11/Main_24263.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main_24263 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        System.out.print(br.readLine() + "\n" + 1);
+
+        br.close();
+        bw.close();
+    }
+}
